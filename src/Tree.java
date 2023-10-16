ff07f567920d4545e633ed728f1e008ab584d86f
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-10-16 08:17:52 +0700

    tree constructor

--- src/Tree.java
+++ src/Tree.java
@@ -26,22 +26,56 @@ public class Tree {
             for(int i = 0; i < 8; i++){
                 for(int j = 0; j < 8; j++){
                     if (buttons[i][j].getText().equals("")){
-                        treeList.add(new Tree(updateButtons(buttons, h, h_init), h-1, h_init, i_tree, j_tree));
+                        treeList.add(new Tree(updateButtons(buttons, h, h_init, i_tree, j_tree), h-1, h_init, i_tree, j_tree));
                     }
                 }
             }
         }
     }
 
-    public Button[][] updateButtons(Button[][] oldButtons, int h, int h_init){
+    public Button[][] updateButtons(Button[][] oldButtons, int h, int h_init, int i_tree, int j_tree){
         Button[][] newButtons = oldButtons;
         if ((h_init-h)%2 == 0){
             // giliran O
-            newButton[][]
+            newButtons[i_tree][j_tree].setText("O");
+            // cek atas
+            if (i_tree-1 > 0 && newButtons[i_tree-1][j_tree].getText().equals("X")){
+                newButtons[i_tree-1][j_tree].setText("O");
+            }
+            // cek bawah
+            if (i_tree+1 < 8 && newButtons[i_tree+1][j_tree].getText().equals("X")){
+                newButtons[i_tree+1][j_tree].setText("O");
+            }
+            // cek kiri
+            if (j_tree-1 > 0 && newButtons[i_tree][j_tree-1].getText().equals("X")){
+                newButtons[i_tree][j_tree-1].setText("O");
+            }
+            // cek kanan
+            if (j_tree+1 < 8 && newButtons[i_tree][j_tree+1].getText().equals("X")){
+                newButtons[i_tree][j_tree+1].setText("O");
+            }
         }
         else{
-
+            // giliran X
+            newButtons[i_tree][j_tree].setText("X");
+            // cek atas
+            if (i_tree-1 > 0 && newButtons[i_tree-1][j_tree].getText().equals("O")){
+                newButtons[i_tree-1][j_tree].setText("X");
+            }
+            // cek bawah
+            if (i_tree+1 < 8 && newButtons[i_tree+1][j_tree].getText().equals("O")){
+                newButtons[i_tree+1][j_tree].setText("X");
+            }
+            // cek kiri
+            if (j_tree-1 > 0 && newButtons[i_tree][j_tree-1].getText().equals("O")){
+                newButtons[i_tree][j_tree-1].setText("X");
+            }
+            // cek kanan
+            if (j_tree+1 < 8 && newButtons[i_tree][j_tree+1].getText().equals("O")){
+                newButtons[i_tree][j_tree+1].setText("X");
+            }
         }
+        return newButtons;
     }
 
     public int solve(){
