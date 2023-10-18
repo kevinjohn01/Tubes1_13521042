8db23b3147655035193e30365fefdaec8df1dab3
kevinjohn01 <16521168@std.stei.itb.ac.id>
2023-10-18 07:25:14 +0700

    feat: add alpha beta pruning to minimax

--- src/BotMiniMax.java
+++ src/BotMiniMax.java
@@ -1,6 +1,8 @@
 import javafx.scene.control.Button;
 
 public class BotMiniMax extends Bot{
+    private int alpha;
+    private int beta;
     public int[] move(Button[][] buttons, int round) {
         // initial value max_val
         int i_max = firstEmpty(buttons)[0];
@@ -11,21 +13,51 @@ public class BotMiniMax extends Bot{
                 buttons0[i][j] = new Button();
             }
         }
+        this.alpha = Integer.MIN_VALUE;
+        this.beta = Integer.MAX_VALUE;
         buttons0 = updateButton(buttons, i_max, j_max);
-        Tree t0 = new Tree(buttons0, round,i_max, j_max, true);
-        int max_val = t0.getVal();
 
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                if (buttons[i][j].getText().isEmpty()){
-                    Tree t = new Tree(buttons, round, i, j, true);
-                    if (t.getVal() > max_val){
+        //Tree t0 = new Tree(buttons0, round,i_max, j_max, true, alpha, beta);
+        int max_val = Integer.MIN_VALUE;
+
+
+        int maxEval = Integer.MIN_VALUE;
+//        for(int i = 0; i < 8; i++){
+//            for(int j = 0; j < 8; j++){
+//                if (buttons[i][j].getText().isEmpty()) {
+//                    Tree t = new Tree(buttons, round, i, j, false, alpha, beta);
+//                    maxEval = Math.max(t.getAlpha(),t.getVal());
+//                    this.alpha = Math.max(t.getBeta(), t.getVal());
+//                    this.beta = t.getBeta();
+//                    if (t.getVal() >= max_val) {
+//                        max_val = t.getVal();
+//                        i_max = i;
+//                        j_max = j;
+//                    }
+//                    if (this.alpha >= this.beta) {
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+        int i=0;
+        int j=0;
+        while (i<8 && this.alpha < this.beta) {
+            while (j<8 && this.alpha < this.beta){
+                if (buttons[i][j].getText().isEmpty()) {
+                    Tree t = new Tree(buttons, round, i, j, false, alpha, beta);
+                    maxEval = Math.max(t.getAlpha(), t.getVal());
+                    this.alpha = Math.max(t.getBeta(), t.getVal());
+                    this.beta = t.getBeta();
+                    if (t.getVal() >= max_val) {
                         max_val = t.getVal();
                         i_max = i;
                         j_max = j;
                     }
                 }
+                j++;
             }
+            i++;
         }
         return new int[]{i_max,j_max};
     }
--- src/Tree.java
+++ src/Tree.java
@@ -6,13 +6,14 @@ import javafx.application.Application;
 public class Tree {
     private int val;
     private ArrayList<Tree> treeList;
+    private int alpha;
+    private int beta;
 
     public Tree(Button[][] buttons, int h, int i_tree, int j_tree, boolean giliransaya){
         System.out.println("h: " + h);
         treeList = new ArrayList<Tree>();
         // hitung value sendiri di h = 1
         if (h == 1){
-            System.out.println("..");
             buttons = updateButtons(buttons, i_tree, j_tree, giliransaya);
             int nX = 0;
             int nO = 0;
@@ -33,7 +34,6 @@ public class Tree {
         }
         // rekursif buat tree child
         else{
-            System.out.println("...");
             buttons = updateButtons(buttons,i_tree,j_tree,giliransaya);
             for(int i = 0; i < 8; i++){
                 for(int j = 0; j < 8; j++){
@@ -52,8 +52,102 @@ public class Tree {
         }
     }
 
+    public Tree(Button[][] buttons, int h, int i_tree, int j_tree, boolean giliransaya, int alpha, int beta){
+        System.out.println("h: " + h);
+        treeList = new ArrayList<Tree>();
+        // hitung value sendiri di h = 1
+        if (h == 1){
+            System.out.println("..");
+            buttons = updateButtons(buttons, i_tree, j_tree, giliransaya);
+            int nX = 0;
+            int nO = 0;
+            for(int i = 0; i < 8; i++){
+                for(int j = 0; j < 8; j++){
+                    if (buttons[i][j].getText().equals("X")){
+                        nX++;
+                    }
+                    if (buttons[i][j].getText().equals("O")){
+                        nO++;
+                    }
+                }
+            }
+            System.out.println(i_tree + " " + j_tree + " Buttons: ");
+            printButtons(buttons);
+            this.val = nO-nX;
+            System.out.println("val: " + this.val);
+
+            //assign value alpha beta
+            this.alpha = alpha;
+            this.beta = beta;
+        }
+        // rekursif buat tree child
+        else{
+            if (giliransaya) {
+                int maxEval = Integer.MIN_VALUE;
+                System.out.println("...");
+                buttons = updateButtons(buttons, i_tree, j_tree, true);
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        if (buttons[i][j].getText().isEmpty()) {
+                            Tree updatedTree = new Tree(buttons, h - 1, i, j, false, alpha, beta);
+                            maxEval = Math.max(maxEval, updatedTree.getVal());
+                            System.out.println("maxEval:"+maxEval);
+                            this.alpha = Math.max(updatedTree.getAlpha(), updatedTree.getVal());
+                            this.beta = updatedTree.getBeta();;
+
+                            if (this.alpha >= this.beta) {//pruning
+                                System.out.println("kena prune di " + h);
+                                break;
+                            } else {
+                                this.treeList.add(updatedTree);
+                                System.out.println("berhasil ditambahkan");
+                            }
+                        }
+                    }
+                }
+            } else {
+                int minEval = Integer.MAX_VALUE;
+                System.out.println("...");
+                buttons = updateButtons(buttons, i_tree, j_tree, false);
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        if (buttons[i][j].getText().isEmpty()) {
+                            Tree updatedTree = new Tree(buttons, h - 1, i, j, true, alpha, beta);
+                            minEval = Math.min(minEval, updatedTree.getVal());
+                            System.out.println("maxEval:"+minEval);
+
+                            this.beta = Math.min(updatedTree.getBeta(), updatedTree.getVal());
+                            this.alpha = updatedTree.getAlpha();
+
+                            if (this.alpha >= this.beta) {//pruning
+                                System.out.println("kena prune di " + h);
+                                break;
+                            } else {
+                                this.treeList.add(updatedTree);
+                                System.out.println("berhasil ditambahkan");
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println("else " + this.val);
+        }
+
+        System.out.println("alpha: "+this.alpha);
+        System.out.println("beta: "+this.beta);
+    }
+
     public int getVal(){
         return this.val;
+    }
+
+    public int getAlpha(){
+        return this.alpha;
+    }
+
+    public int getBeta(){
+        return this.beta;
     }
 
     public void setVal(int value) {
